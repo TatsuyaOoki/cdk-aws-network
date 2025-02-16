9c8707929fb6a71ec10f8e5d4954b0877ca56501
TatsuyaOoki <tat.ooki@outlook.jp>
2025-02-16 14:29:11 +0000

    firstcommit

--- .projenrc.ts
+++ .projenrc.ts
@@ -5,9 +5,11 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   name: 'cdk-aws-network',
   projenrcTs: true,
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: ['dotenv'], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.addGitIgnore('.env');
 project.synth();
