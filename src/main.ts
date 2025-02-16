a503dd59d4a8f94276ad27e62e4e329276bf536e
TatsuyaOoki <tat.ooki@outlook.jp>
2025-02-16 14:32:28 +0000

    Comment out non-network sections

--- src/main.ts
+++ src/main.ts
@@ -49,66 +49,66 @@ export class MyStack extends Stack {
     });
 
     /* ============ KeyPair ============ */
-    const keyPair = new ec2.KeyPair(this, 'KeyPair', {});
+    // const keyPair = new ec2.KeyPair(this, 'KeyPair', {});
 
 
     /* ============ InstanceProfile ============ */
-    const InstanceRole = new iam.Role(this, 'InstanceRole', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
-      path: '/',
-      managedPolicies: [
-        { managedPolicyArn: 'arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore' },
-        { managedPolicyArn: 'arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy' },
-      ],
-    });
+    // const InstanceRole = new iam.Role(this, 'InstanceRole', {
+    //   assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    //   path: '/',
+    //   managedPolicies: [
+    //     { managedPolicyArn: 'arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore' },
+    //     { managedPolicyArn: 'arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy' },
+    //   ],
+    // });
 
     /* ============ EC2 Instance ============ */
-    const linuxSg = new ec2.SecurityGroup(this, 'linuxSg', {
-      vpc: vpc,
-      allowAllOutbound: true,
-    });
+    // const linuxSg = new ec2.SecurityGroup(this, 'linuxSg', {
+    //   vpc: vpc,
+    //   allowAllOutbound: true,
+    // });
 
-    const linuxAmi = ec2.MachineImage.lookup({
-      name: 'al2023-ami-2023.6.20241212.0-kernel-6.1-x86_64',
-      owners: [
-        'amazon',
-        accountId,
-      ],
-    });
+    // const linuxAmi = ec2.MachineImage.lookup({
+    //   name: 'al2023-ami-2023.6.20241212.0-kernel-6.1-x86_64',
+    //   owners: [
+    //     'amazon',
+    //     accountId,
+    //   ],
+    // });
 
-    const linuxUserdata = ec2.UserData.forLinux({ shebang: '#!/bin/bash' });
-    linuxUserdata.addCommands(
-      'sudo dnf -y install httpd',
-      'sudo echo "<h1>Hello from $(hostname)</h1>" > /var/www/html/index.html',
-      'sudo chown apache:apache /var/www/html/index.html',
-      'sudo systemctl enable httpd',
-      'sudo systemctl start httpd',
-    );
+    // const linuxUserdata = ec2.UserData.forLinux({ shebang: '#!/bin/bash' });
+    // linuxUserdata.addCommands(
+    //   'sudo dnf -y install httpd',
+    //   'sudo echo "<h1>Hello from $(hostname)</h1>" > /var/www/html/index.html',
+    //   'sudo chown apache:apache /var/www/html/index.html',
+    //   'sudo systemctl enable httpd',
+    //   'sudo systemctl start httpd',
+    // );
 
-    new ec2.Instance(this, 'linuxInstance', {
-      vpc: vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
-      machineImage: linuxAmi,
-      role: InstanceRole,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC,
-      },
-      securityGroup: linuxSg,
-      userData: linuxUserdata,
-      keyPair: keyPair,
-      blockDevices: [
-        {
-          deviceName: '/dev/xvda',
-          volume: ec2.BlockDeviceVolume.ebs(20, {
-            encrypted: true,
-            volumeType: ec2.EbsDeviceVolumeType.GP3,
-          }),
-        },
-      ],
-    });
-    linuxSg.addIngressRule(eicSg, ec2.Port.SSH);
-    linuxSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.HTTP);
-    eicSg.addEgressRule(linuxSg, ec2.Port.SSH);
+    // new ec2.Instance(this, 'linuxInstance', {
+    //   vpc: vpc,
+    //   instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+    //   machineImage: linuxAmi,
+    //   role: InstanceRole,
+    //   vpcSubnets: {
+    //     subnetType: ec2.SubnetType.PUBLIC,
+    //   },
+    //   securityGroup: linuxSg,
+    //   userData: linuxUserdata,
+    //   keyPair: keyPair,
+    //   blockDevices: [
+    //     {
+    //       deviceName: '/dev/xvda',
+    //       volume: ec2.BlockDeviceVolume.ebs(20, {
+    //         encrypted: true,
+    //         volumeType: ec2.EbsDeviceVolumeType.GP3,
+    //       }),
+    //     },
+    //   ],
+    // });
+    // linuxSg.addIngressRule(eicSg, ec2.Port.SSH);
+    // linuxSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.HTTP);
+    // eicSg.addEgressRule(linuxSg, ec2.Port.SSH);
 
   }
 }
