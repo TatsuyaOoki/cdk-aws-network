1579739c6e3714ac5f2c953e7b35503944e3d0f8
TatsuyaOoki <tat.ooki@outlook.jp>
2025-02-25 13:36:57 +0000

    update: speparation code

---
+++ src/lib/stacks/app-stack.ts
@@ -0,0 +1,19 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Network } from '../constructs/network';
+
+export interface AppStackProps extends StackProps {
+  vpcCidr: string;
+}
+
+export class AppStack extends Stack {
+  constructor(scope: Construct, id: string, props: AppStackProps) {
+    super(scope, id, props);
+
+    new Network(this, 'Network', {
+      vpcCidr: props.vpcCidr,
+    });
+
+    
+  }
+}
